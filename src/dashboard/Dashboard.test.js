c804c229c2b54cc9731168bea11a7d39a8b9c8f3
chefboyrdeuce <jpldeuce@gmail.com>
2020-01-17 20:24:41 -0500

    completed Snapshot testing on controls, dashboard, and display

---
+++ src/dashboard/Dashboard.test.js
@@ -0,0 +1,13 @@
+//Snapshot Testing
+import React from 'react';
+import renderer from 'react-test-renderer';
+
+import Dashboard from './Dashboard';
+
+describe('<Dashboard />', () => {
+  it('should match snapshot', () => {
+    const tree = renderer.create(<Dashboard />).toJSON();
+
+    expect(tree).toMatchSnapshot();
+  });
+});
---
+++ src/display/Display.test.js
@@ -0,0 +1,13 @@
+//Snapshot Testing
+import React from 'react';
+import renderer from 'react-test-renderer';
+
+import Display from './Display';
+
+describe('<Display />', () => {
+  it('should match snapshot', () => {
+    const tree = renderer.create(<Display />).toJSON();
+
+    expect(tree).toMatchSnapshot();
+  });
+});

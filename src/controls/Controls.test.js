704b4c51fe08aa6d8c6fbb54388315ecc8851549
chefboyrdeuce <jpldeuce@gmail.com>
2020-01-17 20:20:25 -0500

    added react-test-renderer to conduct Snapshot testing

---
+++ src/controls/Controls.test.js
@@ -0,0 +1,13 @@
+//Snapshot Testing
+import React from 'react';
+import renderer from 'react-test-renderer';
+
+import Controls from './Controls';
+
+describe('<Controls />', () => {
+  it('should match snapshot', () => {
+    const tree = renderer.create(<Controls />).toJSON();
+
+    expect(tree).toMatchSnapshot();
+  });
+});
